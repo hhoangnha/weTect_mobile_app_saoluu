e4bb2e648d365e98829fd9a450fc44489201be80
nhanl3715 <nhanl3715@gmail.com>
2020-08-25 17:49:17 +0700

    saoluu3

--- src/navigations/AppNavigations.js
+++ src/navigations/AppNavigations.js
@@ -16,7 +16,7 @@ export default class AppNavigations extends Component {
         const Stack = createStackNavigator()
         return (
             <NavigationContainer>
-                <StatusBar translucent={true} backgroundColor={'transparent'} barStyle={"dark-content"}/>
+                <StatusBar translucent={true} backgroundColor={'transparent'} barStyle={"default"}/>
                 <Stack.Navigator screenOptions={{headerShown:false}}>
                     {/* <Stack.Screen name="Login" component={ScreenLogin} />
                     <Stack.Screen name="Register" component={ScreenRegister} /> */}
--- src/screens/Home/Page/HomePage.js
+++ src/screens/Home/Page/HomePage.js
@@ -1,37 +1,45 @@
 import React, {useRef} from 'react'
-import { View, Text, Button,StyleSheet, Image, ScrollView, Dimensions,TouchableOpacity } from 'react-native'
+import { View, Text, Button,StyleSheet, Image, ScrollView, Dimensions,TouchableOpacity, ImageBackground } from 'react-native'
 import { SearchBar } from 'react-native-elements';
+import { FlatList } from 'react-native-gesture-handler';
+import MyCarousel from './../Carousel/Carousel'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-export default function HomePage({navigation}) {
 
-    const slideShow = [
-      "https://cf.shopee.vn/file/e6adbe7551c1bfdc9639c67672b5863b",
-      "https://cf.shopee.vn/file/e6adbe7551c1bfdc9639c67672b5863b"
-    ]
-    return (
-      <View style={styles.container}>
-          <View style={styles.header}>
-            <View style={styles.boxSearch}>
-              <View style={{width:"10%"}}>
-                <Text>|||</Text>
-              </View>
-              <View style={{width:"80%"}}>
-                <SearchBar
-                  containerStyle={{padding:0}}
-                  lightTheme
-                  placeholder="Type Here..."
-                  onChangeText={()=>{}}
-                  value={'hello'}
-                />
-                
-              </View>
-              
-            </View>
-              
-          </View>
-        
-      </View>
-    )
+export default function HomePage(){
+      const slideShow = [
+        "https://cf.shopee.vn/file/e6adbe7551c1bfdc9639c67672b5863b",
+        "https://cf.shopee.vn/file/e6adbe7551c1bfdc9639c67672b5863b"
+      ]
+      const image = { uri: "https://reactjs.org/logo-og.png" };
+      return (
+        <View style={styles.container}>
+            <ImageBackground source={require("./../../../../public/background.jpg")} style={styles.image}>
+              <ScrollView>
+                <View style={styles.header}>
+                  <View style={styles.boxSearch}>
+                    <View style={{width:"10%", alignItems:"center", justifyContent:"center"}}>
+                      <Text>|||</Text>
+                    </View>
+                    <View style={{width:"90%"}}>
+                      <SearchBar
+                        containerStyle={{height:45, padding:0, backgroundColor:"transparent", justifyContent:"center"}}
+                        inputContainerStyle={{height:35,backgroundColor:"white"}}
+                        lightTheme
+                        placeholder="Tìm kiếm"
+                        onChangeText={()=>{}}
+                      />
+                      
+                    </View>
+                  </View>
+                  
+                </View>
+                <MyCarousel/>
+              </ScrollView>
+            </ImageBackground>
+            
+        </View>
+      )
 }
 
 const styles = StyleSheet.create({
@@ -40,10 +48,22 @@ const styles = StyleSheet.create({
     
   },
   header:{
-    flex:0.3,
-    backgroundColor:'#48BE6F'
+    flex:0.1,
+    justifyContent:"center", alignItems:"center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
   },
   boxSearch:{
     width:"100%",
+    flexDirection:"row",
+    zIndex:9,
+    padding:10,
+    paddingTop:30,
+    
+    // borderBottomLeftRadius:100,
+    borderBottomRightRadius:100
   }
 })
